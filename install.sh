a2cd53e7e1e5067d43fabfbbed5a25b84a5522e3
Antti Linno <antti@begin.ee>
2021-12-17 07:59:58 +0200

    Remove part script and use one script to rule them all.

--- install.sh
+++ install.sh
@@ -2,15 +2,67 @@
 
 set -o nounset                                  # Treat unset variables as an error
 
-if [ -f ".env" ]; then
-  . .env
-else
-  echo "No .env file found!"
-fi
+# Create primary partition
+echo ';' | sfdisk /dev/sda
 
-./pre-chroot.sh
+# Create fs
+mkfs.ext4 -L system /dev/sda1
 
-# Run rest of the commands in chroot
-cp .env /mnt
-cp chroot.sh /mnt
-arch-chroot /mnt ./chroot.sh
+# Sync clock
+timedatectl set-ntp true
+
+# Mount partition to /mnt
+mount /dev/sda1 /mnt
+
+# Strap base system
+pacstrap /mnt base linux linux-firmware
+
+# Write fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+cat <<EOF > /mnt/root/part2.sh
+# Timezone
+ln -sf /usr/share/zoneinfo/Europe/Tallinn /etc/localtime
+
+# Sync time
+hwclock --systohc
+
+# Generate locale
+sed -i -e 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i -e 's/#et_EE.UTF-8 UTF-8/et_EE.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+
+# Set hostname
+echo "blank" > /etc/hostname
+
+echo "root:$ROOT_PASSWORD" | chpasswd
+
+# Install system packages
+pacman -S --noconfirm neovim git syslinux openssh
+
+# Configure network
+systemctl enable systemd-networkd.service
+systemctl enable sshd
+
+cat <<'NET' > /etc/systemd/network/20-wired.network
+[Match]
+Name=enp0s3
+
+[Network]
+DHCP=yes
+NET
+
+# Configure boot loader
+syslinux-install_update -i -a -m
+sed -i -e 's/sda3/sda1/' /boot/syslinux/syslinux.cfg
+
+exit # to leave the chroot
+EOF
+
+chmod a+x /mnt/root/part2.sh
+
+arch-chroot /mnt /root/part2.sh
+
+# Stuff here to do after exiting the chroot ... reboot perhaps.
+# ...
